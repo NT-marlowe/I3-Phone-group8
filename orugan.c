11c9f280d6f0253e94089e871fae64a70910061c
NT-marlowe <n.tezuka196.ninj@gmail.com>
2021-06-21 17:07:30 +0900

    added orugan_sound_make

---
+++ orugan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+double orugan_sound(double sound_freq, const int sample_freq, const int n) {
+  
+  double res = 0; 
+
+  double a[5] = {};
+ 
+  a[0] = 1.0 * exp(-5.0 * n / (sample_freq * 4.0));
+  a[1] = 0.8 * exp(-5.0 * n / (sample_freq * 2.0));
+  a[2] = 0.6 * exp(-5.0 * n / (sample_freq * 1.0));
+  a[3] = 0.5 * exp(-5.0 * n / (sample_freq * 0.5));
+  a[4] = 0.4 * exp(-5.0 * n / (sample_freq * 0.2));
+
+  for (int i = 0; i < 5; i++) {
+    res += a[i] * sin(2.0 * M_PI * sound_freq * (i+1) * n / sample_freq);
+  }
+  
+  return res;
+  
+}
