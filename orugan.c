0885925691642e7e8794cfe0f93b9b4f61392ee4
NT-marlowe <n.tezuka196.ninj@gmail.com>
2021-06-22 13:32:32 +0900

    modified orugan.c

--- orugan.c
+++ orugan.c
@@ -9,18 +9,18 @@ signed short orugan_sound(const signed short A, const double f, const int fs, co
 
   double a[5] = {};
  
-  a[0] = 1.0 * exp(-5.0 * n / (sample_freq * 4.0));
-  a[1] = 0.8 * exp(-5.0 * n / (sample_freq * 2.0));
-  a[2] = 0.6 * exp(-5.0 * n / (sample_freq * 1.0));
-  a[3] = 0.5 * exp(-5.0 * n / (sample_freq * 0.5));
-  a[4] = 0.4 * exp(-5.0 * n / (sample_freq * 0.2));
+  a[0] = 1.0 * exp(-5.0 * n / (fs * 4.0));
+  a[1] = 0.8 * exp(-5.0 * n / (fs * 2.0));
+  a[2] = 0.6 * exp(-5.0 * n / (fs * 1.0));
+  a[3] = 0.5 * exp(-5.0 * n / (fs * 0.5));
+  a[4] = 0.4 * exp(-5.0 * n / (fs * 0.2));
 
   for (int i = 0; i < 5; i++) {
-    res += a[i] * sin(2.0 * M_PI * sound_freq * (i+1) * n / sample_freq);
+    res += a[i] * sin(2.0 * M_PI * f * (i+1) * n / fs);
   }
   
   res *= A;
-  
+
   return (signed short)res;
   
 }
