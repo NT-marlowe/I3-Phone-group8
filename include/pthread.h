1aa9f04d2b49e90994e0796bbffc0c193e4bac14
NT-marlowe <n.tezuka196.ninj@gmail.com>
2021-07-02 17:05:33 +0900

    added send_function

--- include/pthread.h
+++ include/pthread.h
@@ -5,11 +5,14 @@ struct args_pthread
 {
   int s; // file descriptor for send and recv
   signed short *buf; // pointer of the head of data array
+  int fail;
 };
+
+typedef struct args_pthread Args_pthread;
 
 void *receive_data_from_client(void *arg);
 
-void sum_waves(signed short **waves, signed short *result);
+void sum_waves(signed short **waves, signed short *result, int number_of_client);
 
 void *send_data_to_client(void *arg);
 
