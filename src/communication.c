a65e7e959f1582d9287e7a2f9534c1137572ae87
ItoShun-Utokyo <72190483+ItoShun-Utokyo@users.noreply.github.com>
2021-07-03 17:39:02 +0900

    Add files via upload

--- src/communication.c
+++ src/communication.c
@@ -44,13 +44,12 @@ void server(int port,int number_of_client, int *s){
 
   //このソケットを用いたすべてのread、recvに対してタイムアウトを設定する
   //タイムアウトしたときのerrnoは11
-  struct timespec tsp;
-  tsp.tv_sec = 0;
-  tsp.tv_nsec = (long)(N / 44100 * 1e9);  // 掛け算の順番が重要．1e9を超えないような計算順序にする．
-  // fprintf(stderr, "tv_sec = %ld, tv_nsec = %ld\n", tsp.tv_sec, tsp.tv_nsec);
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 1e6*1024/44100;
   for (int i = 0; i < number_of_client; i++) {
-    int opt = setsockopt(s[i], SOL_SOCKET, SO_RCVTIMEO, (void *)&tsp, (socklen_t)sizeof(struct timespec));
-    if (opt == -1) die("setstockopt");
+    setsockopt(s[i], SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv));
+    if (setsockopt(s[i], SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv)) == -1) die("setsockopt");
   }
   
   //接続が完了したら各クライアントにダミーデータ(1)を送る
